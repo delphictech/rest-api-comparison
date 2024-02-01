27ab6327936efec2717dcb27a8c724496de10cca
Seth Yates <95386742+SethY8s@users.noreply.github.com>
2024-02-01 17:27:42 -0500

    working first route

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+// #![allow(unused)]
+
+
+
+use axum::{response::Html, routing::get, Router};
+use tokio::net::TcpListener;
+
+
+#[tokio::main]
+async fn main() {
+     // Define the routes
+     let app = Router::new().route(
+        "/hello",
+        get(|| async { Html("Hello,<strong> World! </strong>") }),
+    );
+
+    // Define the address and port
+    
+
+    let listener = TcpListener::bind("0.0.0.0:8000").await.unwrap();
+	println!("->> LISTENING on {:?}\n", listener.local_addr());
+	axum::serve(listener, app).await.unwrap();
+
 }
