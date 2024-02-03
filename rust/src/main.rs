d3444b543610a0ff6cf94e5a38cf4e655bfbc15a
Seth Yates <95386742+SethY8s@users.noreply.github.com>
2024-02-03 14:50:08 -0500

    imoprted handlers function into main.rs

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,24 +1,21 @@
 // #![allow(unused)]
 
-
-
 use axum::{response::Html, routing::get, Router};
 use tokio::net::TcpListener;
 
+mod handlers; // Import the handlers module
+
+use handlers::my_function;
 
 #[tokio::main]
 async fn main() {
-     // Define the routes
-     let app = Router::new().route(
-        "/",
-        get(|| async { Html("HELLO WORLD") }),
-    );
+    // Define the routes
+    let app = Router::new()
+        .route("/", get(|| async { Html("HELLO WORLD") }))
+        .route("/test", get(my_function()));
 
     // Define the address and port
-    
-
     let listener = TcpListener::bind("0.0.0.0:8000").await.unwrap();
-	println!("->> LISTENING on {:?}\n", listener.local_addr());
-	axum::serve(listener, app).await.unwrap();
-
+    println!("->> LISTENING on {:?}\n", listener.local_addr());
+    axum::serve(listener, app).await.unwrap();
 }
