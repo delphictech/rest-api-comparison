fd7992df8691886794a4f813fef65ef9a1d8212b
toni-akintola <oakintol@nd.edu>
2024-02-14 18:33:08 -0500

    passing all unit tests

--- python/app.py
+++ python/app.py
@@ -1,7 +1,44 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, Response
 
 app = Flask(__name__)
 
+loginDetails = {
+    "alex": {
+        "authToken": "123",
+        "userName": "alex",
+    },
+    "jason": {
+        "authToken": "234",
+        "userName": "jason",
+    },
+    "marie": {
+        "authToken": "345",
+        "userName": "marie",
+    },
+}
+coinDetails = {
+    "alex": {
+        "balance": 100,
+    },
+    "jason": {
+        "balance": 200,
+    },
+    "marie": {
+        "balance": 300,
+    },
+}
+
+
+@app.before_request
+def validateUser():
+
+    path = request.path
+    headers = request.headers
+    route = path.split("/")
+    if route[1] and route[1] == "coins":
+        name = route[-1]
+        if not headers.get("Authtoken") or not loginDetails[name]:
+            return Response({"response": {"status": 403}}, status=403, mimetype='application/json')
 
 @app.route("/")
 def hello_world():
@@ -16,5 +53,9 @@ def return_json():
 
 
 @app.route("/coins/<userID>", methods=["GET", "POST"])
-def tokens():
-    pass
+def tokens(userID):
+    return jsonify({"data": {"balance": coinDetails[userID]["balance"]}})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
