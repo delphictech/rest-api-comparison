f8cd51c9c38699f2fa86584262a89bb902110c74
toni-akintola <oakintol@nd.edu>
2024-02-14 22:33:21 -0500

    documentation and README.md

--- python/app.py
+++ python/app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request, Response
 
 app = Flask(__name__)
 
+# Filler login & user details
 loginDetails = {
     "alex": {
         "authToken": "123",
@@ -31,10 +32,15 @@ coinDetails = {
 
 @app.before_request
 def validateUser():
-
+    '''
+    Here, we set up a middleware that parses the request route 
+    and validates the user if they are making a request for coin data
+    '''
     path = request.path
     headers = request.headers
+
     route = path.split("/")
+
     if route[1] and route[1] == "coins":
         name = route[-1]
         if not headers.get("Authtoken") or not loginDetails[name]:
@@ -47,6 +53,10 @@ def hello_world():
 
 @app.get("/test")
 def return_json():
+    '''
+    Flask comes with many json parsing methods/functions that 
+    considerably increase quality of life
+    '''
     json = {"message": "testing route", "code": 200}
 
     return jsonify(json)
@@ -54,6 +64,10 @@ def return_json():
 
 @app.route("/coins/<userID>", methods=["GET", "POST"])
 def tokens(userID):
+    '''
+    If the request passes the middleware,
+    we return a JSON object with the user's current balance of coins
+    '''
     return jsonify({"data": {"balance": coinDetails[userID]["balance"]}})
 
 
